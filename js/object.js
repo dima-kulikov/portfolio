b47fdd18d7f5259e0db5ceb8b72603fd462dc961
Dima <skaizy44@gmail.com>
2023-01-23 23:05:22 +0200

    gtgt
    gtgt

--- js/object.js
+++ js/object.js
@@ -17,27 +17,35 @@ const projectList = [
     },
     {
         img: './img/projects/project-03.jpg',
-        title: 'Video portal',
-        desc: 'html',
-        filter: "Commerce"
+        title: 'Black Friday',
+        desc: 'html, Gulp, Sass, JS, CSS animation(transition/@keyframes)',
+        filter: "Commerce",
+        git: 'https://github.com/dima-kulikov/Black-friday-DM',
+        site: 'https://dima-kulikov.github.io/Black-friday-DM/build/index.html'
     },
     {
         img: './img/projects/project-04.jpg',
-        title: 'Dating app',
-        desc: 'html',
-        filter: "Commerce"
+        title: 'Sub Surf',
+        desc: 'html, CSS',
+        filter: "Landing",
+        git: 'https://github.com/dima-kulikov/sub-surf-landing-page',
+        site: 'https://dima-kulikov.github.io/sub-surf-landing-page/'
     },
     {
         img: './img/projects/project-05.jpg',
-        title: 'Landing',
-        desc: 'html',
-        filter: "Landing"
+        title: 'Start UP',
+        desc: 'html, Gulp, Sass, JS',
+        filter: "Landing",
+        git: 'https://github.com/dima-kulikov/Startup-landing2',
+        site: 'https://dima-kulikov.github.io/Startup-landing2/build/index.html'
     },
     {
         img: './img/projects/project-06.jpg',
-        title: 'Gaming community',
-        desc: 'html, html',
-        filter: "React JS"
+        title: 'Sport betting',
+        desc: 'html, Gulp, JS, CSS animation(transition/@keyframes)',
+        filter: "Landing",
+        git: 'https://github.com/dima-kulikov/sport-betting',
+        site: 'https://dima-kulikov.github.io/sport-betting/build/index.html'
     }
 ]
 
