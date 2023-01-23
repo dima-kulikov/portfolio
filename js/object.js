30c751e1fd6e78e8238c07c2e6c62f07d8dbc65f
Dima <skaizy44@gmail.com>
2023-01-23 22:13:09 +0200

    рнр
    нрн

--- js/object.js
+++ js/object.js
@@ -1,32 +1,42 @@
 const projectList = [
     {
         img: './img/projects/project-01.jpg',
-        title: 'Gaming streaming portal',
-        filter: "Landing"
+        title: 'Cooperation with the military page',
+        desc: 'html, Gulp, Sass, JS, Swiper Slider',
+        filter: "Commerce",
+        git: '',
+        site: 'https://dnipro-m.ua/volonteram-zsu/'
     },
     {
         img: './img/projects/project-02.jpg',
-        title: 'Video service',
-        filter: "Landing"
+        title: 'delivery and payment page',
+        desc: 'html, Gulp, Sass, JS',
+        filter: "Commerce",
+        git: '',
+        site: 'https://dnipro-m.ua/dostavka-i-oplata/'
     },
     {
         img: './img/projects/project-03.jpg',
         title: 'Video portal',
+        desc: 'html',
         filter: "Commerce"
     },
     {
         img: './img/projects/project-04.jpg',
         title: 'Dating app',
+        desc: 'html',
         filter: "Commerce"
     },
     {
         img: './img/projects/project-05.jpg',
         title: 'Landing',
+        desc: 'html',
         filter: "Landing"
     },
     {
         img: './img/projects/project-06.jpg',
         title: 'Gaming community',
+        desc: 'html, html',
         filter: "React JS"
     }
 ]
--- js/projects.js
+++ js/projects.js
@@ -4,14 +4,62 @@ import projectList from './object.js';
 let projects = document.getElementById('projects');
 let projectsFilter = document.querySelector('.projects__filter');
 const projectFilterItem = document.querySelectorAll('.projects__filter--item');
-
+let projectModalClose = document.querySelector('.project__modal--close');
 
 // функция отрисовывает все блоки
 function postAllprojects(obj) {
 const allProjectList = obj.map((el) =>{
     let li = document.createElement('li');
     li.classList.add('project');
-    let a = document.createElement('a');
+   
+    // создания модального окна
+    let divModal = document.createElement('div');
+    divModal.classList.add('project__modal');
+    
+    let closeModal = document.createElement('div');
+    closeModal.classList.add('project__modal--close');
+
+
+    let imgModal = document.createElement('img');
+    imgModal.classList.add('project__modal--img');
+    imgModal.src = el.img;
+
+    let pModal = document.createElement('p');
+    pModal.innerHTML = el.desc;
+   
+
+    let divTwoBtn = document.createElement('div');
+    divTwoBtn.classList.add('project__modal--btn');
+
+
+    if(el.git !== ''){
+    let aModalGit = document.createElement('a');
+    aModalGit.classList.add('btn-outline');
+    aModalGit.href = el.git;
+    aModalGit.innerHTML = 'GitHub repo';
+    aModalGit.target = "_blank";
+    divTwoBtn.appendChild(aModalGit);
+}
+
+    let aModalSite = document.createElement('a');
+    aModalSite.classList.add('btn-outline');
+    aModalSite.href = el.site;
+    aModalSite.innerHTML = 'Site online';
+    aModalSite.target = "_blank";
+
+
+
+    divModal.appendChild(closeModal);
+    divModal.appendChild(imgModal);
+    divModal.appendChild(pModal);
+    
+    divTwoBtn.appendChild(aModalSite);
+    divModal.appendChild(divTwoBtn);
+    
+    
+
+
+    // карточка
     let img = document.createElement('img');
     img.classList.add('project__img');
     img.src = el.img;
@@ -19,13 +67,17 @@ const allProjectList = obj.map((el) =>{
     h3.classList.add('project__title');
     h3.innerHTML = el.title;
     
-    li.appendChild(a);
-    a.appendChild(img);
-    a.appendChild(h3);
+
+    
+    li.appendChild(divModal);
+
+
+    
+    li.appendChild(img);
+    li.appendChild(h3);
     projects.appendChild(li);
 })
 };
-
 postAllprojects(projectList);
 
 
@@ -35,30 +87,60 @@ function toggleActiveClass(){
         projectFilterItem[i].classList.remove('projects__filter--item--active');
     }
 }
+// фильтрует проэкты по кнопкам
+function filterItemBtn(element){
+    let result = projectList.filter(el => el.filter == element);
+    return result;
+}
 
 // ставлю слушатель и выбираю что вывести на страницу
 projectsFilter.addEventListener('click', (e) => {
-    
 
-
-
-    if(e.target.innerHTML == 'Landing'){
-       let result = projectList.filter(el => el.filter == 'Landing');
-       projects.innerHTML = '';
-       postAllprojects(result);
-       toggleActiveClass()
-       e.target.classList.add('projects__filter--item--active')
-    }else if(e.target.innerHTML == 'Commerce'){
-        let result = projectList.filter(el => el.filter == 'Commerce');
+switch (e.target.innerHTML) {
+    case 'All projects':
         projects.innerHTML = '';
-        postAllprojects(result);
-        toggleActiveClass()
-        e.target.classList.add('projects__filter--item--active')
-     }else if(e.target.innerHTML == 'React JS'){
-        let result = projectList.filter(el => el.filter == 'React JS');
+        postAllprojects(projectList);
+        toggleActiveClass();
+        e.target.classList.add('projects__filter--item--active');
+      break;
+
+    case 'Landing':
         projects.innerHTML = '';
-        postAllprojects(result);
-        toggleActiveClass()
-        e.target.classList.add('projects__filter--item--active')
-     }
+        postAllprojects(filterItemBtn(e.target.innerHTML));
+        toggleActiveClass();
+        e.target.classList.add('projects__filter--item--active');
+    break;
+
+    case 'Commerce':
+        projects.innerHTML = '';
+        postAllprojects(filterItemBtn(e.target.innerHTML));
+        toggleActiveClass();
+        e.target.classList.add('projects__filter--item--active');
+      break;
+
+    case 'React JS':
+        projects.innerHTML = '';
+        postAllprojects(filterItemBtn(e.target.innerHTML));
+        toggleActiveClass();
+        e.target.classList.add('projects__filter--item--active');
+      break;
+
+    default:
+        break;
+  }
+});
+
+
+// слушатель на карточки для открытия модального окна
+projects.addEventListener('click', (e) => {
+
+    console.log(e.target)
+    if( e.target.classList.contains('project__img')){
+        e.target.parentNode.childNodes[0].classList.add('project__modal--open');
+    }else if(e.target.classList.contains('project__modal--close')){
+        const projectModal = document.querySelectorAll('.project__modal')
+        for(let i = 0; i < projectModal.length; i++){
+            projectModal[i].classList.remove('project__modal--open')
+        }
+    }
 })
